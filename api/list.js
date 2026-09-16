2f98d0e0f12a57640c371f01a154036ca5ca2f0b
ZyxHex <zyaghoib@gmail.com>
2026-09-16 14:15:42 +0700

    Add API handler for licenses with CORS support

---
+++ api/list.js
@@ -0,0 +1,16 @@
+let licenses = global.licenses || [];
+global.licenses = licenses;
+
+export default function handler(req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+    if (req.method === 'OPTIONS') return res.status(200).end();
+
+    return res.status(200).json({
+        success: true,
+        total: licenses.length,
+        data: licenses
+    });
+}
